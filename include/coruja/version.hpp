544672ad5458194952d9d2379188e6cef2f624a2
Ricardo Cosme <r@cosme.im>
2018-10-17 12:32:47 -0300

    Adds version.hpp

---
+++ include/coruja/version.hpp
@@ -0,0 +1,9 @@
+
+// Copyright Ricardo Calheiros de Miranda Cosme 2018.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#define CORUJA_VERSION "0.1.0_alpha"
